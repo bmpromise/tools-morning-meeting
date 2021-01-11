4cac47f02dc27ca8b966d69760e507fcc1a3bc4f
bmpromise <52149398+bmpromise@users.noreply.github.com>
2021-01-11 13:18:27 +0800

    fixed bug

--- main.js
+++ main.js
@@ -4,78 +4,89 @@ let output = document.getElementById('oup');
 console.log('inp=>',input,'\n','out=>',output);
 input.addEventListener('input',inputFn);
 
-let objAry = [];
-let txtAry = ['更新内容：','更新内容:','服务端：','服务端:','客户端：','客户端:','Lob, ']
-let nameMap = [];
+const deleAry = ['Lob, ']; // 直接剔除input 內容
+const txtAry = ['更新内容：','更新内容:','服务端：','服务端:','客户端：','客户端:']; // 來源屏蔽
+
+let objAry = []; // 最後成品
+let nameMap = []; // 不重複名單
+
 function inputFn(e){
+  console.clear();
   let textAry = [];
   let arr = String(input.value).split('\n');
-  let ary =[];
 
   // init
   objAry = [];
   nameMap = [];
 
-  arr.forEach((el,ind) =>{
-    txtAry.forEach(el2 =>{
-      el.search(el2)>-1 && ary.push(ind)
-    })
-  })
+  /**
+   * 第一階段處理 - 特定詞排除與 更新input訊息
+   **/
 
-  ary.forEach((e,i)=>{
-    arr.splice(e-i,1)
-  })
+  // 排除 telegram 複製為Lob的訊息 (deleAry)
+  deleAry.forEach(el=>{
+    arr = filterTxtFn(arr,el);
+  });
   
-  // --------------------------------------
-  // 建議 再處理XD
-  let txt = arr.join('\n');
-  textAry = txt.split('\n\n');
+  // input update
+  input.value = arr.join('\n');
 
-  textAry.forEach(((ele,ind)=>{
+  // 屏蔽-更新內容 服務端 客戶端
+  txtAry.forEach(el =>{
+    arr = filterTxtFn(arr,el);
+  });
+
+  /**
+   * 第二階段處理
+   * 組裝 objAry[Obj] => List
+   * 辨識 nameMap 參與人員
+   * 產生 output 內容
+   **/
+  textAry = arr.join('\n').split('\n\n');
+
+  textAry = textAry.filter(x => x.length > 0)
+  console.log('Start-----------objAry--------------Start')
+  textAry.forEach((ele,ind)=>{
     let s = ele.split('\n');
-    if(s[0].length==0) s.splice(0,1); // 去掉一次多餘行數
+    // 去除空行
+    s = s.filter(x=>x.length>0);
+
     let s1 = s[0].indexOf('-');
     let s2 = s[0].lastIndexOf('-')+1;
+    // 尋找產品 與 名稱 與 備註
     let title = s[0].slice(0,s1);
     let name = s[0].slice(s2);
-    let content = s.slice(1).join()
+    let content = s.slice(1).join();
+    // 預留objAry[obj] => List
     let obj = {title, name, content};
     objAry.push(obj);
-    addMap(name)
-    console.log('objAry[',ind,']=>',objAry[ind])
-  }))
-  // --------------------------------------
-  txxx('nameMap=>',nameMap);
+    addMap(name);
+    console.log('objAry[',ind,']=>',objAry[ind]);
+  });
+  console.log('END-----------objAry--------------END')
+  
+  // 組裝
   let d = new Date();
-
+  nameMap.push('lob');
+  nameMap.unshift('ice');
+  console.log('nameMap=>',nameMap);
   let tit = `会议主题: 每日代码交流会议
 时间: ${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()} 10:00
-参会人员: ice, ${nameMap.join(', ')}, lob
+参会人员: ${nameMap.join(', ')}
 会议主持: `
 
   output.value = tit;
-  txxx('objAry=>',objAry)
+  console.log('objAry=>',objAry);
 }
 
+// filter
+function filterTxtFn(arr,str){
+  return arr.filter(x => x.search(str) < 0);
+}
+// map
 function addMap(txt){
-  let bo = false
-  nameMap.forEach(el=>{
-    if(txt == el) bo=true;
-  })
-  if(bo) return
+  nameMap = nameMap.filter(x => txt!=x);
   nameMap.push(txt);
 }
-function txxx(txt , obj){
-  console.log(txt,obj);
-}
-
-
 
 input.focus();
-
-/* 
-会议主题: 每日代码交流会议
-时间: 2021-1-8 10:00
-参会人员: ice, wendy, carlson, nick, rosco, ray, burt, lob
-会议主持: Su
-*/
